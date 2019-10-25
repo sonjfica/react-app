17d62eeec9cd088352dfb4f68bd9bbb0aac681f2
髙橋次郎 <sonjfica.work@gmail.com>
2019-10-25 09:20:46 +0900

    create class components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type='text' onChange={( console.log('clicked'))} />
-      </React.Fragment>
-    )
-  }
+// Appクラスがコンポネントを継承して作成されている　クラスコンポーネント
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type='text' onChange={( console.log('clicked'))} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  ) 
 }
+
+const Cat = () => {
+  return <div>Meow</div>
+} 
 
 export default App;
